205565098806b94e51f7b301a05b28078efcaf00
Cecelia Kaufmann <ceceiekaufmann@gmail.com>
2021-04-30 17:16:57 -0500

    started working on spacebar class

--- src/spacegame/GameSetUp.java
+++ src/spacegame/GameSetUp.java
@@ -1,28 +1,38 @@
 package spacegame;
-
-import edu.macalester.graphics.*;
 import java.awt.Color;
 
+import edu.macalester.graphics.*;
+import edu.macalester.graphics.events.KeyboardEvent;
+
 public class GameSetUp {
-    private static final int CANVAS_WIDTH = 1280;
+    private static final int CANVAS_WIDTH = 600;
     private static final int CANVAS_HEIGHT = 800;
 
     private CanvasWindow canvas;
     private ShooterFriend ballObject1;
+    private Laser testLaser;
+    private Alien alien;
 
 
     public GameSetUp(){
         canvas = new CanvasWindow("Space Shooter!", CANVAS_WIDTH, CANVAS_HEIGHT);
         canvas.setBackground(Color.BLACK);
-    //     ballObject1 = new ShooterFriend(300, 400, 7, 600, 800, canvas);
-    //    ballObject1.addToCanvas(canvas);
-    //    Laser testLaser = new Laser(300, 300, 300, 350, 3);
-    //    canvas.add(testLaser);
-    //     canvas.animate(()->testLaser.moveLaser(canvas));
-        Alien alien = new Alien(300, 400);
-        alien.addToCanvas(canvas);
-    
+        // ballObject1 = new ShooterFriend (300, 570, 7, 600, 800, canvas);
+        // ballObject1.addToCanvas(canvas);
+        // ballObject1.addToCanvas(canvas);
+        //Laser testLaser = new Laser(300, 300, 3);
+        //Laser testLaser = new Laser(300, 300, 300, 350, 3);
+        // canvas.add(testLaser);
+        //canvas.onMouseMove(event -> shooterFriend.shooterPosition(event.getPosition()));
+        // canvas.animate(()->testLaser.moveLaser(canvas));
+        // Alien alien = new Alien(300, 400);
+
     }
+
+    // public void action(KeyboardEvent e){
+    //     if (e.getKeyCode()==KeyEvent.VK_SPACE){
+
+    //     }
 
 
     
