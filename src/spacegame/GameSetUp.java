fd0080903ea9c96d43f5bcaa254c1062f0c2ee53
Cecelia Kaufmann <ceceiekaufmann@gmail.com>
2021-05-01 17:58:30 -0500

    need a shoot method

--- src/spacegame/GameSetUp.java
+++ src/spacegame/GameSetUp.java
@@ -13,19 +13,21 @@ public class GameSetUp {
     private Player player;
     private Laser testLaser;
     private Alien alien;
+    
 
 
     public GameSetUp(){
         canvas = new CanvasWindow("Space Shooter!", CANVAS_WIDTH, CANVAS_HEIGHT);
         canvas.setBackground(Color.BLACK);
-        // ballObject1 = new ShooterFriend (300, 570, 7, 600, 800, canvas);
-        // ballObject1.addToCanvas(canvas);
+        ballObject1 = new ShooterFriend (300, 570, 7, 600, 800, canvas);
+        ballObject1.addToCanvas(canvas);
         // ballObject1.addToCanvas(canvas);
         //Laser testLaser = new Laser(300, 300, 3);
         //Laser testLaser = new Laser(300, 300, 300, 350, 3);
         // canvas.add(testLaser);
-        //canvas.onMouseMove(event -> shooterFriend.shooterPosition(event.getPosition()));
-        // canvas.animate(()->testLaser.moveLaser(canvas));
+        //canvas.onMouseMove(event -> ballObject1.updatePosition(event.getPosition()));
+        //canvas.onKeyUp(event -> player.updatePosition());
+        canvas.animate(()->testLaser.moveLaser(canvas));
         Alien alien = new Alien(640, 400);
         alien.addToCanvas(canvas);
         Player player = new Player(640, 450);
