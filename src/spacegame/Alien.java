bbd20e678310008f7ed2da8633c58eb0eff859fd
Emydius <emydiusmontes@gmail.com>
2021-05-02 02:24:54 -0700

    Set up Alien auto-shooting code
    I left a note in the Alien class about an error.

--- src/spacegame/Alien.java
+++ src/spacegame/Alien.java
@@ -1,9 +1,15 @@
 package spacegame;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import edu.macalester.graphics.Image;
 
 public class Alien extends Sprite {
     
+    private Instant start;
+    private Instant end;
+
     public Alien(double x, double y) {
         super(x, y);
         setImage(new Image(x, y, "sprites/armedalien.png"));
@@ -19,11 +25,21 @@ public class Alien extends Sprite {
     }
 
     /**
-     * Not implemented yet
+     * Makes the alien shoot a laser every 3 seconds.
      */
     @Override
     public void animationHandler() {
-
+        start = Instant.now();
+        getImage().getCanvas().animate(() -> {
+            end = Instant.now();
+            if (Duration.between(start, end).toSecondsPart() >= 3) {
+                // shootLaser(); // This code works perfectly and everything in the if is executed every 3 seconds.
+                // However, I can't call shootLaser() or else it throws a java.lang.reflect.InvocationTargetException
+                // and a java.util.ConcurrentModificationException? Idk what is going on and so I just decided to go sleep.
+                System.out.println("Alien shoots a laser!");
+                start = Instant.now();
+            }
+        });
     }
 
 }
